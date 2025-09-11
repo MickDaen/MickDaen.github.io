2ecc9cefc6cc38ec82ce8e5acd358014c4a920fd
Mickaël Daeninckx <mickael.daeninckx@gmail.com>
2025-09-11 11:50:53 +0200

    Modifications mineures : ajustement des contenus

--- js/realisations.js
+++ js/realisations.js
@@ -1,11 +1,11 @@
 const classeur = document.querySelectorAll(".classeur");
 const realisations = document.querySelectorAll(".wallimage");
 const tout = document.querySelector("#real-all");
-const flyer = document.querySelector("#real-flyer");
-const brochure = document.querySelector("#real-brochure");
-const reseaux = document.querySelector("#real-reseaux");
 const logos = document.querySelector("#real-logo");
-const animation = document.querySelector("#real-animation");
+const prints = document.querySelector("#real-print");
+const edition = document.querySelector("#real-edition");
+const reseaux = document.querySelector("#real-reseaux");
+const illustration = document.querySelector("#real-illustration");
 
 for (i = 0; i < classeur.length; i++) {
   classeur[i].addEventListener("click", (e) => {
@@ -29,89 +29,89 @@ for (i = 0; i < classeur.length; i++) {
 tout.addEventListener("click", () => {
   tout.style.background = "#1a0bf0";
   tout.style.color = "white";
-  flyer.style.background = "none";
-  flyer.style.color = "#0A0A0A";
-  brochure.style.background = "none";
-  brochure.style.color = "#0A0A0A";
+  logos.style.background = "none";
+  logos.style.color = "#0A0A0A";
+  prints.style.background = "none";
+  prints.style.color = "#0A0A0A";
+  edition.style.background = "none";
+  edition.style.color = "#0A0A0A";
   reseaux.style.background = "none";
   reseaux.style.color = "#0A0A0A";
-  logos.style.background = "none";
-  logos.style.color = "#0A0A0A";
-  animation.style.background = "none";
-  animation.style.color = "#0A0A0A";
-});
-
-flyer.addEventListener("click", () => {
-  tout.style.background = "none";
-  tout.style.color = "#0A0A0A";
-  flyer.style.background = "#1a0bf0";
-  flyer.style.color = "white";
-  brochure.style.background = "none";
-  brochure.style.color = "#0A0A0A";
-  reseaux.style.background = "none";
-  reseaux.style.color = "#0A0A0A";
-  logos.style.background = "none";
-  logos.style.color = "#0A0A0A";
-  animation.style.background = "none";
-  animation.style.color = "#0A0A0A";
-});
-
-brochure.addEventListener("click", () => {
-  tout.style.background = "none";
-  tout.style.color = "#0A0A0A";
-  flyer.style.background = "none";
-  flyer.style.color = "#0A0A0A";
-  brochure.style.background = "#1a0bf0";
-  brochure.style.color = "white";
-  reseaux.style.background = "none";
-  reseaux.style.color = "#0A0A0A";
-  logos.style.background = "none";
-  logos.style.color = "#0A0A0A";
-  animation.style.background = "none";
-  animation.style.color = "#0A0A0A";
-});
-
-reseaux.addEventListener("click", () => {
-  tout.style.background = "none";
-  tout.style.color = "#0A0A0A";
-  flyer.style.background = "none";
-  flyer.style.color = "#0A0A0A";
-  brochure.style.background = "none";
-  brochure.style.color = "#0A0A0A";
-  reseaux.style.background = "#1a0bf0";
-  reseaux.style.color = "white";
-  logos.style.background = "none";
-  logos.style.color = "#0A0A0A";
-  animation.style.background = "none";
-  animation.style.color = "#0A0A0A";
+  illustration.style.background = "none";
+  illustration.style.color = "#0A0A0A";
 });
 
 logos.addEventListener("click", () => {
   tout.style.background = "none";
   tout.style.color = "#0A0A0A";
-  flyer.style.background = "none";
-  flyer.style.color = "#0A0A0A";
-  brochure.style.background = "none";
-  brochure.style.color = "#0A0A0A";
-  reseaux.style.background = "none";
-  reseaux.style.color = "#0A0A0A";
   logos.style.background = "#1a0bf0";
   logos.style.color = "white";
-  animation.style.background = "none";
-  animation.style.color = "#0A0A0A";
-});
-
-animation.addEventListener("click", () => {
-  tout.style.background = "none";
-  tout.style.color = "#0A0A0A";
-  flyer.style.background = "none";
-  flyer.style.color = "#0A0A0A";
-  brochure.style.background = "none";
-  brochure.style.color = "#0A0A0A";
+  prints.style.background = "none";
+  prints.style.color = "#0A0A0A";
+  edition.style.background = "none";
+  edition.style.color = "#0A0A0A";
   reseaux.style.background = "none";
   reseaux.style.color = "#0A0A0A";
+  illustration.style.background = "none";
+  illustration.style.color = "#0A0A0A";
+});
+
+prints.addEventListener("click", () => {
+  tout.style.background = "none";
+  tout.style.color = "#0A0A0A";
   logos.style.background = "none";
   logos.style.color = "#0A0A0A";
-  animation.style.background = "#1a0bf0";
-  animation.style.color = "white";
+  prints.style.background = "#1a0bf0";
+  prints.style.color = "white";
+  edition.style.background = "none";
+  edition.style.color = "#0A0A0A";
+  reseaux.style.background = "none";
+  reseaux.style.color = "#0A0A0A";
+  illustration.style.background = "none";
+  illustration.style.color = "#0A0A0A";
+});
+
+edition.addEventListener("click", () => {
+  tout.style.background = "none";
+  tout.style.color = "#0A0A0A";
+  logos.style.background = "none";
+  logos.style.color = "#0A0A0A";
+  prints.style.background = "none";
+  prints.style.color = "#0A0A0A";
+  edition.style.background = "#1a0bf0";
+  edition.style.color = "white";
+  reseaux.style.background = "none";
+  reseaux.style.color = "#0A0A0A";
+  illustration.style.background = "none";
+  illustration.style.color = "#0A0A0A";
+});
+
+reseaux.addEventListener("click", () => {
+  tout.style.background = "none";
+  tout.style.color = "#0A0A0A";
+  logos.style.background = "none";
+  logos.style.color = "#0A0A0A";
+  prints.style.background = "none";
+  prints.style.color = "#0A0A0A";
+  edition.style.background = "none";
+  edition.style.color = "#0A0A0A";
+  reseaux.style.background = "#1a0bf0";
+  reseaux.style.color = "white";
+  illustration.style.background = "none";
+  illustration.style.color = "#0A0A0A";
+});
+
+illustration.addEventListener("click", () => {
+  tout.style.background = "none";
+  tout.style.color = "#0A0A0A";
+  logos.style.background = "none";
+  logos.style.color = "#0A0A0A";
+  prints.style.background = "none";
+  prints.style.color = "#0A0A0A";
+  edition.style.background = "none";
+  edition.style.color = "#0A0A0A";
+  reseaux.style.background = "none";
+  reseaux.style.color = "#0A0A0A";
+  illustration.style.background = "#1a0bf0";
+  illustration.style.color = "white";
 });
